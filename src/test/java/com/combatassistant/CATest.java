b8d6691ee9e72cccc5ee2e066310a248dcf3b63d
Anta <j0siddy@gmail.com>
2025-08-17 07:37:49 -0500

    Basic customization from template

--- src/test/java/com/combatassistant/CATest.java
+++ src/test/java/com/combatassistant/CATest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.combatassistant;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class CATest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(CAPlugin.class);
 		RuneLite.main(args);
 	}
 }
